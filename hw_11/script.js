8e7a71663dc506ff8761481ba64d389ea01fd5c3
panikite <ne.isnuje@gmail.com>
2020-06-02 18:49:48 +0300

    add hw_11 / first animation

---
+++ hw_11/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    let jedy = document.getElementById('jedy_button'),
+        l1 = document.getElementById('l1'),
+        l2 = document.getElementById('l2'),
+        l3 = document.getElementById('l3');
+
+
+    jedy.addEventListener('click', function() {
+       l1.classList.toggle('l1_active');
+       l2.classList.toggle('l2_active');
+       l3.classList.toggle('l3_active');
+    });
+   
+
+});
+
+
