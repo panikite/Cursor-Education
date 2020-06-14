6b144b785ceca87b94109cd5fd61df5489de29c5
panikite <ne.isnuje@gmail.com>
2020-06-14 19:53:12 +0300

    fix html before preloader

--- hw_14/js/main.js
+++ hw_14/js/main.js
@@ -1,14 +1,13 @@
 
-
 $(window).on('load', () => {
     $(".preloader-container").fadeOut(2500);
+    $('#body').css("display", "block");
     $('#mobile-video').attr('src', './img/video_mobile.mp4');
     
-    $(document.head).append('<link rel="stylesheet" href="css/main-min.css">');
+    $(document.head).append('<link rel="stylesheet" href="css/main.css">');
     // $(document.body).append('<link rel="preload" href="css/main-min.css" as="style">');
     // $(document.head).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
     // $(document.body).append('<script type="text/javascript" src="./slick-1.8.1/slick/slick.min.js"></script>');
-    
    
     $('.review-slider').slick({
         prevArrow: $(".bg-arrow_left"),
