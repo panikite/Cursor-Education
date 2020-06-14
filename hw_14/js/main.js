1bd50a13c881b70c003f1220225221d6905e6729
panikite <ne.isnuje@gmail.com>
2020-06-14 19:34:42 +0300

    fonts, slider conn. changes

--- hw_14/js/main.js
+++ hw_14/js/main.js
@@ -1,14 +1,12 @@
-// $(document.body).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
-// $(document.body).append('<script type="text/javascript" src="./slick-1.8.1/slick/slick.min.js"></script>');
-
-// $(document.head).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
 
 
 $(window).on('load', () => {
+    $(".preloader-container").fadeOut(2500);
     $('#mobile-video').attr('src', './img/video_mobile.mp4');
-    $(".preloader-container").fadeOut(1500);
-    $(document.body).append('<link rel="stylesheet" href="css/main-min.css">');
-    // $(document.heady).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
+    
+    $(document.head).append('<link rel="stylesheet" href="css/main-min.css">');
+    // $(document.body).append('<link rel="preload" href="css/main-min.css" as="style">');
+    // $(document.head).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
     // $(document.body).append('<script type="text/javascript" src="./slick-1.8.1/slick/slick.min.js"></script>');
     
    
