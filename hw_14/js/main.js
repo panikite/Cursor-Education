0bc1f97e70b43227919cce6d576e9787c570401f
panikite <ne.isnuje@gmail.com>
2020-06-14 16:51:21 +0300

    convert video - thumb from png to webp

--- hw_14/js/main.js
+++ hw_14/js/main.js
@@ -1,6 +1,5 @@
-// $(document.body).append('<link rel="stylesheet" href="css/main-min.css">');
+$(document.body).append('<link rel="stylesheet" href="css/main-min.css">');
 $(window).on('load', () => {
-    // $('#video').attr('src', 'https://www.youtube.com/embed/M7lc1UVf-VE');
     $('#mobile-video').attr('src', './img/video_mobile.mp4');
     $(".preloader-container").fadeOut(1500);
 
