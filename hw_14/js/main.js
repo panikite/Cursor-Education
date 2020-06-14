acda59e91ecf3af09531f3e68214dc7f446c09a3
panikite <ne.isnuje@gmail.com>
2020-06-14 18:12:17 +0300

    add slider

--- hw_14/js/main.js
+++ hw_14/js/main.js
@@ -1,7 +1,23 @@
-$(document.body).append('<link rel="stylesheet" href="css/main-min.css">');
+// $(document.body).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
+// $(document.body).append('<script type="text/javascript" src="./slick-1.8.1/slick/slick.min.js"></script>');
+
+// $(document.head).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
+
+
 $(window).on('load', () => {
     $('#mobile-video').attr('src', './img/video_mobile.mp4');
     $(".preloader-container").fadeOut(1500);
+    $(document.body).append('<link rel="stylesheet" href="css/main-min.css">');
+    // $(document.heady).append('<link rel="stylesheet" type="text/css" href="./slick-1.8.1/slick/slick.css"/>');
+    // $(document.body).append('<script type="text/javascript" src="./slick-1.8.1/slick/slick.min.js"></script>');
+    
+   
+    $('.review-slider').slick({
+        prevArrow: $(".bg-arrow_left"),
+        nextArrow: $(".bg-arrow_right")
+    });
+    
+
 
 
 
